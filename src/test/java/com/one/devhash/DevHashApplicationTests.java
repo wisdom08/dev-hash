81724831fdf37804aa8cbe32e16ddb5ef583a166
wisdom08 <wisdom08@kakao.com>
2022-08-12 14:01:17 +0900

    feat: 프로젝트 셋업

---
+++ src/test/java/com/one/devhash/DevHashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.devhash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
