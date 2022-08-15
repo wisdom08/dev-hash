9bda81bedf23f00226602bae6f3f244b1a7f268a
wisdom08 <wisdom08@kakao.com>
2022-08-15 14:03:44 +0900

    feat: #4 추가 커밋 넣은 부분 롤백

--- src/main/java/com/one/devhash/security/config/WebSecurityConfig.java
+++ src/main/java/com/one/devhash/security/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/api/chats/**").access("isAuthenticated()")
                 .antMatchers(HttpMethod.PUT,"/api/chats/**").access("isAuthenticated()")
                 .antMatchers(HttpMethod.DELETE,"/api/chats/**").access("isAuthenticated()")
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
 
                 .and()
                 .apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
