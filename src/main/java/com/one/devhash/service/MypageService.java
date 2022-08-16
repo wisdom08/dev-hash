0bda174dc6171a883ec1c757d097890f9b00aaa4
mediumorange <ujeorange@gmail.com>
2022-08-16 19:28:28 +0900

    fix: #19 조회시 오류 수정

--- src/main/java/com/one/devhash/service/MypageService.java
+++ src/main/java/com/one/devhash/service/MypageService.java
@@ -1,30 +1,35 @@
 package com.one.devhash.service;
 
+import com.one.devhash.domain.ImageTarget;
+import com.one.devhash.domain.Product;
 import com.one.devhash.domain.User;
-import com.one.devhash.dto.chatting.RoomResponseDto;
-import com.one.devhash.dto.mypage.MypageResponseDto;
 import com.one.devhash.dto.product.ProductResponseDto;
+import com.one.devhash.dto.user.UserResponseDto;
+import com.one.devhash.repository.ImagefileRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class MypageService {
 	private final UserService userService;
-	private final ProductService productService;
-	private final RoomService roomService;
+	private final ImagefileRepository imagefileRepository;
 
-	public MypageResponseDto getMypage() {
+	public UserResponseDto getMypage() {
 		User user = userService.findByUserName(getCurrentUsername());
-		List<ProductResponseDto> productList = productService.getMypageProduct(user.getId());
-		return MypageResponseDto.builder()
-				.user(user)
-				.productList(productList)
-				.build();
+		List<Product> products = user.getProducts();
+		List<ProductResponseDto> productDtos = products.stream()
+				.map(p -> new ProductResponseDto(p, imagefileRepository.findAllByTargetId(ImageTarget.PRODUCT, p.getProductId())))
+				.collect(Collectors.toList());
+		UserResponseDto userDto = new UserResponseDto(user, productDtos);
+		return userDto;
 	}
 
 	private static String getCurrentUsername() {
