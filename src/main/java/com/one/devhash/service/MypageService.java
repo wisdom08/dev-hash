eb28bc6084200bda29f73282db56089d1b3b400f
wisdom08 <wisdom08@kakao.com>
2022-08-18 03:17:40 +0900

    fix: 영속성 컨텍스트 관련 에러 발생 문제로 `@Transactional` 어노테이션 추가
    org.hibernate.LazyInitializationException

--- src/main/java/com/one/devhash/service/MypageService.java
+++ src/main/java/com/one/devhash/service/MypageService.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
