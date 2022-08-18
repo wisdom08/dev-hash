09b8e80a6d8084ef648fed0918d1258b97f1b196
mediumorange <ujeorange@gmail.com>
2022-08-18 15:22:30 +0900

    fix: 상품 삭제시 작성자 확인 오류

--- src/main/java/com/one/devhash/controller/WishController.java
+++ src/main/java/com/one/devhash/controller/WishController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class WishController {
 	private final WishService wishService;
 
-	@ApiOperation(value = "찜 추가", notes = "찜 목록에 상품을 추가합니다.")
+	@ApiOperation(value = "찜 추가/취소", notes = "찜 목록에 상품을 추가하거나 삭제합니다.")
 	@PostMapping("/api/wish/{productId}")
 	public CommonResponse<?> addWish(@PathVariable Long productId) {
 		wishService.isWish(productId);
